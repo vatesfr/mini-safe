f15de107a1236b58f9d5559bd62bdfe31d80557c
heafalan <natacha.heafala@gmail.com>
2018-10-05 12:29:37 +0200

    server: create/delete/update/list entries

--- packages/server/main.js
+++ packages/server/main.js
@@ -6,9 +6,73 @@ import { format, parse } from "json-rpc-protocol";
 // see https://koajs.com/
 const app = new Koa();
 
+var entries = new Map();
+let idCounter = 0;
+
 const METHODS = {
   subtract([a, b]) {
     return a - b;
+  },
+
+  createEntry([nameEntry, contentEntry]) {
+    const entry = {
+      id: idCounter,
+      name: nameEntry,
+      content: contentEntry,
+      created: Date.now(),
+      updated: Date.now(),
+    };
+
+    idCounter += 1;
+    entries.set(entry.id, entry);
+
+    return entry.name + " created";
+  },
+
+  listEntries() {
+    let res = "";
+    entries.forEach(function(entry) {
+      res +=
+        "(" +
+        entry.id +
+        ") " +
+        entry.name +
+        " " +
+        entry.content +
+        " " +
+        entry.created +
+        " " +
+        entry.updated +
+        "\n";
+    });
+    return res;
+  },
+
+  deleteEntry([idEntry]) {
+    const entry = entries.get(+idEntry);
+    if (entry === undefined) {
+      return "This entry doesn't exist !";
+    }
+
+    entries.delete(+idEntry);
+    return "(" + entry.id + ") " + entry.name + " is deleted";
+  },
+
+  updateEntry([idEntry, nameEntry, contentEntry]) {
+    let res = "This entry doesn't exist !";
+    if (nameEntry === undefined || contentEntry === undefined) {
+      return "params ? new name & new content expected";
+    } else {
+      entries.forEach(function(entry) {
+        if (entry.id === +idEntry) {
+          entry.name = nameEntry;
+          entry.content = contentEntry;
+          entry.updated = Date.now();
+          res = idEntry + " is updated";
+        }
+      });
+    }
+    return res;
   },
 };
 
