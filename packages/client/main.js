ee298363063749037e4d8c706bf1c59114dff908
heafalan <natacha.heafala@gmail.com>
2018-10-17 11:25:38 +0200

    fix(client): test with parameters map

--- packages/client/main.js
+++ packages/client/main.js
@@ -12,53 +12,80 @@ import {
 import assert from "assert";
 
 async function main() {
-  const n = process.argv[2];
-  const LISTTESTCASE = [
-    [
-      /* TestCase OK */
-      ["deleteEntries",""],
-      ["listEntries","assert.deepEqual(response.result, [])"],
-      ["createEntry --name name1 --content content1", ""],
-      ["createEntry --name name2 --content content2", ""],
-      ["createEntry --name name3 --content content3", ""],
-      ["listEntries", "assert.deepEqual(response.result[0][1].name, 'name1');" +
-        " assert.deepEqual(response.result[0][1].content, 'content1');" + 
-        " assert.deepEqual(response.result[0][1].id, 1);"],
-      ["updateEntry --id 1 --name name1_modified --content content1_modified",""],
-      ["listEntries", "assert.deepEqual(response.result[0][1].name, 'name1_modified');" +
-        " assert.deepEqual(response.result[0][1].content, 'content1_modified');"],
-      ["deleteEntry --id 2", ""],
-      ["listEntries", "assert.notEqual(response.result[1][0], 2);" ],
-      ["updateEntry --id 3 --name name3_modified", ""],
-      ["listEntries", "assert.deepEqual(response.result[1][1].name, 'name3_modified');" +
-        " assert.deepEqual(response.result[1][1].content, 'content3')"]
-    ]
-  ];
+  var map = new Map();
+  let idCounter = 0;
 
-  const TESTCASE = LISTTESTCASE[n];
+  map.set(idCounter++, { method: "deleteEntries" });
+  map.set(idCounter++, { method: "listEntries" });
+  map.set(idCounter++, {
+    method: "createEntry",
+    params: { name: "name1", content: "content1" },
+  });
+  map.set(idCounter++, {
+    method: "createEntry",
+    params: { name: "name2", content: "content2" },
+  });
+  map.set(idCounter++, {
+    method: "createEntry",
+    params: { name: "name3", content: "content3" },
+  });
+  map.set(idCounter++, {
+    method: "listEntries",
+    test:
+      "assert.deepEqual(response.result[0][1].name, 'name1');" +
+      "assert.deepEquel(response.result[0][1].content, 'content1');",
+  });
+  map.set(idCounter++, {
+    method: "updateEntry",
+    params: { id: 1, name: "name1_modified", content: "content1_modified" },
+  });
+  map.set(idCounter++, {
+    method: "listEntries",
+    test:
+      "assert.deepEqual(response.result[0][1].name, 'name1_modified');" +
+      "assert.deepEqual(response.result[0][1].content, 'content1_modified');",
+  });
+  map.set(idCounter++, { method: "deleteEntry", params: { id: 2 } });
+  map.set(idCounter++, {
+    method: "listEntries",
+    test: "assert.notEqual(response.result[1][0], 2);",
+  });
+  map.set(idCounter++, {
+    method: "updateEntry",
+    params: { id: 3, name: "only_name3_modified" },
+  });
+  map.set(idCounter++, {
+    method: "updateEntry",
+    params: { id: 1, content: "only_content1_modified" },
+  });
+  map.set(idCounter++, {
+    method: "listEntries",
+    test:
+      "assert.deepEqual(response.result[1][1].name, 'only_name3_modified');" +
+      "assert.deepEqual(response.result[1][1].content, 'content3_modified');" +
+      "assert.deepEqual(response.result[0][1].name, 'name1_modified');" +
+      "assert.deepEqual(response.result[0][1].content, 'only_content1_modified');",
+  });
 
-  for (var i = 0; i < TESTCASE.length; i++) {
-    const params = require("minimist")(TESTCASE[i][0].split(" "));
-    const method = params._[0];
-    console.log("\n>>> " + TESTCASE[i][0]);
-    
+  for (var [key, value] of map) {
+    console.log("\n>>> method: " + value.method);
     const response = parse(
       await hrp
         .post("http://localhost:3000/api/", {
-          body: format.request(0, method, params),
+          body: format.request(0, value.method, value.params),
         })
         .readAll("utf-8")
     );
-      
+
     if (response.type === "response") {
       console.log(response.result);
     } else if (response.type === "error") {
       console.error(response.error);
     }
-    
-    if (TESTCASE[i][1] !== "") {
-      console.log("\n>>> " + TESTCASE[i][1]);
-      eval(TESTCASE[i][1]);
+
+    if (value.test !== undefined) {
+      console.log("\n>>> test: " + value.test);
+      new Function("return" + value.test);
     }
   }
 }
