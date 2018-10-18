8593d9e21f0051a61d66536cd4c1d79a9c31c3e0
heafalan <natacha.heafala@gmail.com>
2018-10-18 11:21:49 +0200

    fix(client): tests2

--- packages/client/main.js
+++ packages/client/main.js
@@ -1,87 +1,64 @@
 /* eslint-disable no-console */
 
 import hrp from "http-request-plus";
-import {
-  format,
-  parse,
-  JsonRpcError,
-  InvalidRequest,
-  InvalidParameters,
-  MethodNotFound,
-} from "json-rpc-protocol";
+import { format, parse } from "json-rpc-protocol";
 import assert from "assert";
 
 async function main() {
-  var test = new Function ();
   var TESTCASE = [
-    [
-      "createEntry",
-      { name: "name1", content: "content1" }
-    ],
-    [
-      "createEntry",
-      { name: "name2", content: "content2" }
-    ],
-    [
-      "createEntry",
-      { name: "name3", content: "content3" }
-    ],
+    ["createEntry", { name: "name1", content: "content1" }],
+    ["createEntry", { name: "name2", content: "content2" }],
+    ["createEntry", { name: "name3", content: "content3" }],
     [
       "listEntries",
-      { },
-      test = function(response) {
-        assert.deepEqual(response.result[0][1].name, 'name1');
-        assert.deepEqual(response.result[0][1].content, 'content1');
-        assert.deepEqual(response.result[1][1].name, 'name2');
-        assert.deepEqual(response.result[1][1].content, 'content2');
-        assert.deepEqual(response.result[2][1].name, 'name3');
-        assert.deepEqual(response.result[2][1].content, 'content3');
-      }
+      {},
+      function(response) {
+        assert.deepEqual(response.result[0][1].name, "name1");
+        assert.deepEqual(response.result[0][1].content, "content1");
+        assert.deepEqual(response.result[1][1].name, "name2");
+        assert.deepEqual(response.result[1][1].content, "content2");
+        assert.deepEqual(response.result[2][1].name, "name3");
+        assert.deepEqual(response.result[2][1].content, "content3");
+      },
     ],
     [
       "updateEntry",
-      { id: 1, name: "name1_modified", content: "content1_modified" }
+      { id: 1, name: "name1_modified", content: "content1_modified" },
     ],
     [
       "listEntries",
-      { },
-      test = function(response) {
-        assert.deepEqual(response.result[0][1].name, 'name1_modified');
-        assert.deepEqual(response.result[0][1].content, 'content1_modified');
-      }
+      {},
+      function(response) {
+        assert.deepEqual(response.result[0][1].name, "name1_modified");
+        assert.deepEqual(response.result[0][1].content, "content1_modified");
+      },
     ],
-    [
-      "deleteEntry",
-      { id : 2 }
-    ],
+    ["deleteEntry", { id: 2 }],
     [
       "listEntries",
-      { },
-      test = function(response) {
+      {},
+      function(response) {
         assert.notEqual(response.result[1][0], 2);
-      }
+      },
     ],
-    [
-      "updateEntry",
-      { id: 3, name: "only_name3_modified" }
-    ],
-    [
-      "updateEntry",
-      { id: 1, content: "only_content1_modified" }
-    ],
+    ["updateEntry", { id: 3, name: "only_name3_modified" }],
+    ["updateEntry", { id: 1, content: "only_content1_modified" }],
     [
       "listEntries",
-      { },
-      test = function(response) {
-        assert.deepEqual(response.result[1][1].name, 'only_name3_modified');
-        assert.deepEqual(response.result[1][1].content, 'content3');
-        assert.deepEqual(response.result[0][1].name, 'name1_modified');
-        assert.deepEqual(response.result[0][1].content, 'only_content1_modified');
-      }
-    ]
-  ]
+      {},
+      function(response) {
+        assert.deepEqual(response.result[1][1].name, "only_name3_modified");
+        assert.deepEqual(response.result[1][1].content, "content3");
+        assert.deepEqual(response.result[0][1].name, "name1_modified");
+        assert.deepEqual(
+          response.result[0][1].content,
+          "only_content1_modified"
+        );
+      },
+    ],
+  ];
 
-  for (var i=0; i  < TESTCASE.length; i++ ) {
+  for (var i = 0; i < TESTCASE.length; i++) {
     console.log("\n>>> method: " + TESTCASE[i][0]);
     const response = parse(
       await hrp
