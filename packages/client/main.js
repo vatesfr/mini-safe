70f2cc15bef100a21d863311dc2a3a3b6c5732f5
heafalan <natacha.heafala@gmail.com>
2018-10-18 11:11:47 +0200

    fix(client): tests

--- packages/client/main.js
+++ packages/client/main.js
@@ -12,96 +12,81 @@ import {
 import assert from "assert";
 
 async function main() {
-  const n = process.argv[2];
-  var map = new Map();
-  let idCounter = 0;
-  
-  switch (n) {
-    case '1':
-      map.set(idCounter++, {
-        method: "createEntry",
-        params: { name: "name1", content: "content1" },
-      });
-      map.set(idCounter++, {
-        method: "createEntry",
-        params: { name: "name2", content: "content2" },
-      });
-      map.set(idCounter++, {
-        method: "createEntry",
-        params: { name: "name3", content: "content3" },
-      });
-      map.set(idCounter++, {
-        method: "listEntries",
-        test:
-          "assert.deepEqual(response.result[0][1].name, 'name1');" +
-          "assert.deepEquel(response.result[0][1].content, 'content1');",
-      });
-      map.set(idCounter++, {
-        method: "updateEntry",
-        params: { id: 1, name: "name1_modified", content: "content1_modified" },
-      });
-      map.set(idCounter++, {
-        method: "listEntries",
-        test:
-          "assert.deepEqual(response.result[0][1].name, 'name1_modified');" +
-          "assert.deepEqual(response.result[0][1].content, 'content1_modified');",
-      });
-      map.set(idCounter++, { method: "deleteEntry", params: { id: 2 } });
-      map.set(idCounter++, {
-        method: "listEntries",
-        test: "assert.notEqual(response.result[1][0], 2);",
-      });
-      map.set(idCounter++, {
-        method: "updateEntry",
-        params: { id: 3, name: "only_name3_modified" },
-      });
-      map.set(idCounter++, {
-        method: "updateEntry",
-        params: { id: 1, content: "only_content1_modified" },
-      });
-      map.set(idCounter++, {
-        method: "listEntries",
-        test:
-          "assert.deepEqual(response.result[1][1].name, 'only_name3_modified');" +
-          "assert.deepEqual(response.result[1][1].content, 'content3_modified');" +
-          "assert.deepEqual(response.result[0][1].name, 'name1_modified');" +
-          "assert.deepEqual(response.result[0][1].content, 'only_content1_modified');",
-      });
-      map.set(idCounter++, { method: "deleteEntries" });
-      map.set(idCounter++, { 
-        method: "listEntries",
-        test: "assert.deepEqual(response.result, []);"
-      });
-      break;
-    case '2':  
-      map.set(idCounter++, {
-        method: "createEntry",
-        params: { name: "name1" },
-      });
-      map.set(idCounter++, {
-        method: "listEntries",
-        test: "assert.deepEqual(response.result[0][1].name, 'name1');",
-      });
-      map.set(idCounter++, {
-        method: "deleteEntry",
-        params: { id: 0 },
-        // test: "assert.throws(() => { throw new InvalidParameters(`could not find entry 0`); }, response.error);",
-      });
-      map.set(idCounter++, {
-        method: "listEntries",
-        test: "assert.deepEqual(response.result[0][1].name, 'name1');",
-      });
-      break;
-    default:
-  }
+  var test = new Function ();
+  var TESTCASE = [
+    [
+      "createEntry",
+      { name: "name1", content: "content1" }
+    ],
+    [
+      "createEntry",
+      { name: "name2", content: "content2" }
+    ],
+    [
+      "createEntry",
+      { name: "name3", content: "content3" }
+    ],
+    [
+      "listEntries",
+      { },
+      test = function(response) {
+        assert.deepEqual(response.result[0][1].name, 'name1');
+        assert.deepEqual(response.result[0][1].content, 'content1');
+        assert.deepEqual(response.result[1][1].name, 'name2');
+        assert.deepEqual(response.result[1][1].content, 'content2');
+        assert.deepEqual(response.result[2][1].name, 'name3');
+        assert.deepEqual(response.result[2][1].content, 'content3');
+      }
+    ],
+    [
+      "updateEntry",
+      { id: 1, name: "name1_modified", content: "content1_modified" }
+    ],
+    [
+      "listEntries",
+      { },
+      test = function(response) {
+        assert.deepEqual(response.result[0][1].name, 'name1_modified');
+        assert.deepEqual(response.result[0][1].content, 'content1_modified');
+      }
+    ],
+    [
+      "deleteEntry",
+      { id : 2 }
+    ],
+    [
+      "listEntries",
+      { },
+      test = function(response) {
+        assert.notEqual(response.result[1][0], 2);
+      }
+    ],
+    [
+      "updateEntry",
+      { id: 3, name: "only_name3_modified" }
+    ],
+    [
+      "updateEntry",
+      { id: 1, content: "only_content1_modified" }
+    ],
+    [
+      "listEntries",
+      { },
+      test = function(response) {
+        assert.deepEqual(response.result[1][1].name, 'only_name3_modified');
+        assert.deepEqual(response.result[1][1].content, 'content3');
+        assert.deepEqual(response.result[0][1].name, 'name1_modified');
+        assert.deepEqual(response.result[0][1].content, 'only_content1_modified');
+      }
+    ]
+  ]
 
-
-  for (var [key, value] of map) {
-    console.log("\n>>> method: " + value.method);
+  for (var i=0; i  < TESTCASE.length; i++ ) {
+    console.log("\n>>> method: " + TESTCASE[i][0]);
     const response = parse(
       await hrp
         .post("http://localhost:3000/api/", {
-          body: format.request(0, value.method, value.params),
+          body: format.request(0, TESTCASE[i][0], TESTCASE[i][1]),
         })
         .readAll("utf-8")
     );
@@ -112,9 +97,9 @@ async function main() {
       console.error(response.error);
     }
 
-    if (value.test !== undefined) {
-      console.log("\n>>> test: " + value.test);
-      eval(value.test);
+    if (TESTCASE[i][2] !== undefined) {
+      console.log("\n>>> test: " + TESTCASE[i][2]);
+      TESTCASE[i][2](response);
     }
   }
 }
