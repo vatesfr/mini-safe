401b4d37127771135299bb2fcd6da7fa9ccf7582
Julien Fontanet <julien.fontanet@isonoe.net>
2018-10-24 17:41:06 +0200

    fix(eslint): extends standard-jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: { browser: true, es6: true, node: true },
-  extends: ["standard", "prettier"],
+  extends: ["standard", "standard-jsx", "prettier"],
   parserOptions: {
     ecmaVersion: 8,
     sourceType: "module",
