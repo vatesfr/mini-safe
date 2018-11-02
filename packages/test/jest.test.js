a900e00e156248ecf0065d4d1a255b1040466aac
heafalan <natacha.heafala@gmail.com>
2018-11-02 14:34:09 +0100

    fix(test): use parse.result

--- packages/test/jest.test.js
+++ packages/test/jest.test.js
@@ -4,31 +4,35 @@ const hrp = require("http-request-plus").default;
 const jrp = require("json-rpc-protocol");
 
 async function call(method, params) {
-  return jrp.parse(
-    await hrp
-      .post("http://localhost:4000/api/", {
-        body: jrp.format.request(0, method, params),
-      })
-      .readAll("utf-8")
-  );
+  try {
+    return jrp.parse.result(
+      await hrp
+        .post("http://localhost:4000/api/", {
+          body: jrp.format.request(0, method, params),
+        })
+        .readAll("utf-8")
+    );
+  } catch (error) {
+    return error;
+  }
 }
 
 test("create entry", async () => {
   const id1 = await call("createEntry", { name: "name1", content: "content1" });
-  expect(id1.result).toBe(1);
+  expect(id1).toBe(1);
 
   const id2 = await call("createEntry", { content: "content2" });
-  expect(id2.result).toBe(2);
+  expect(id2).toBe(2);
 
   const id3 = await call("createEntry", { name: "name3" });
-  expect(id3.result).toBe(3);
+  expect(id3).toBe(3);
 
   const response = await call("listEntries");
 
-  expect(response.result[0].name).toBe("name1");
-  expect(response.result[0].content).toBe("content1");
-  expect(response.result[1].content).toBe("content2");
-  expect(response.result[2].name).toBe("name3");
+  expect(response[0].name).toBe("name1");
+  expect(response[0].content).toBe("content1");
+  expect(response[1].content).toBe("content2");
+  expect(response[2].name).toBe("name3");
 });
 
 test("update entry", async () => {
@@ -42,12 +46,12 @@ test("update entry", async () => {
 
   const response = await call("listEntries");
 
-  expect(response.result[0].name).toBe("name1_modified");
-  expect(response.result[0].content).toBe("content1_modified");
-  expect(response.result[1].name).toBe("only_name2_modified");
-  expect(response.result[1].content).toBe("content2");
-  expect(response.result[2].name).toBe("name3");
-  expect(response.result[2].content).toBe("only_content3_modified");
+  expect(response[0].name).toBe("name1_modified");
+  expect(response[0].content).toBe("content1_modified");
+  expect(response[1].name).toBe("only_name2_modified");
+  expect(response[1].content).toBe("content2");
+  expect(response[2].name).toBe("name3");
+  expect(response[2].content).toBe("only_content3_modified");
 });
 
 test("delete entry", async () => {
@@ -55,14 +59,13 @@ test("delete entry", async () => {
 
   const response = await call("listEntries");
 
-  expect(response.result[1]).not.toBe(2);
+  expect(response[1]).not.toBe(2);
 });
 
 test("Error on delete: could not find id 0", async () => {
   const response = await call("deleteEntry", { id: 0 });
 
-  expect(response.type).toBe("error");
-  expect(response.error.message).toBe(new jrp.InvalidParameters().message);
+  expect(response.message).toBe(new jrp.InvalidParameters().message);
 });
 
 test("Error on update: could not find id 0", async () => {
@@ -72,22 +75,19 @@ test("Error on update: could not find id 0", async () => {
     content: "content0",
   });
 
-  expect(response.type).toBe("error");
-  expect(response.error.message).toBe(new jrp.InvalidParameters().message);
+  expect(response.message).toBe(new jrp.InvalidParameters().message);
 });
 
 test("Error on update: name or content expected", async () => {
   const response = await call("updateEntry", { id: 1 });
 
-  expect(response.type).toBe("error");
-  expect(response.error.message).toBe(new jrp.InvalidParameters().message);
+  expect(response.message).toBe(new jrp.InvalidParameters().message);
 });
 
 test("Error: method not found", async () => {
   const response = await call("inexistantMethod");
 
-  expect(response.type).toBe("error");
-  expect(response.error.message).toBe(
+  expect(response.message).toBe(
     new jrp.MethodNotFound("inexistantMethod").message
   );
 });
