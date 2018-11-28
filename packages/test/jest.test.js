63da34eb78c6bbc4a2719dee4cacb10d410e216d
heafalan <natacha.heafala@gmail.com>
2018-11-28 11:32:41 +0100

    feat(test): test notifications (#35)
    Fixes #34

--- packages/test/jest.test.js
+++ packages/test/jest.test.js
@@ -27,36 +27,96 @@ function compareTimestamps(ts1, ts2) {
   return expect(Math.abs(ts1 - ts2)).toBeLessThan(1e2);
 }
 
-let entries, id1, id2, id3;
+let websocket, entries, id1, id2, id3, waitMessage;
+
+beforeAll(async () => {
+  websocket = new WebSocket("ws://localhost:4000");
+
+  let resolveWaitMessage = Function.prototype;
+  waitMessage = () =>
+    new Promise(resolve => {
+      resolveWaitMessage = resolve;
+    });
+
+  websocket.onmessage = event => {
+    resolveWaitMessage(jrp.parse(event.data));
+  };
+
+  await new Promise(resolve => {
+    websocket.onopen = resolve;
+  });
+});
+
+afterAll(() => {
+  websocket.close();
+});
 
 test("create entry", async () => {
-  id1 = await call("createEntry", { name: "name1", content: "content1" });
-  id2 = await call("createEntry", { content: "content2" });
-  id3 = await call("createEntry", { name: "name3" });
+  let message1;
+  [message1, id1] = await Promise.all([
+    waitMessage(),
+    call("createEntry", { name: "name1", content: "content1" }),
+  ]);
+  expect(message1.type).toBe("notification");
+  expect(message1).toEqual({
+    jsonrpc: "2.0",
+    method: "createEntry",
+    params: {
+      entry: {
+        id: id1,
+        name: "name1",
+        content: "content1",
+        created: expect.any(Number),
+        updated: expect.any(Number),
+      },
+    },
+  });
+
+  let message2;
+  [message2, id2] = await Promise.all([
+    waitMessage(),
+    call("createEntry", { content: "content2" }),
+  ]);
+  expect(message2.type).toBe("notification");
+  expect(message2).toEqual({
+    jsonrpc: "2.0",
+    method: "createEntry",
+    params: {
+      entry: {
+        id: id2,
+        content: "content2",
+        created: expect.any(Number),
+        updated: expect.any(Number),
+      },
+    },
+  });
+
+  let message3;
+  [message3, id3] = await Promise.all([
+    waitMessage(),
+    call("createEntry", { name: "name3" }),
+  ]);
+  expect(message3.type).toBe("notification");
+  expect(message3).toEqual({
+    jsonrpc: "2.0",
+    method: "createEntry",
+    params: {
+      entry: {
+        id: id3,
+        name: "name3",
+        created: expect.any(Number),
+        updated: expect.any(Number),
+      },
+    },
+  });
 
   const now = Date.now();
   const response = await call("listEntries");
 
   expect(response).toEqual([
-    {
-      id: id1,
-      name: "name1",
-      content: "content1",
-      created: expect.any(Number),
-      updated: expect.any(Number),
-    },
-    {
-      id: id2,
-      content: "content2",
-      created: expect.any(Number),
-      updated: expect.any(Number),
-    },
-    {
-      id: id3,
-      name: "name3",
-      created: expect.any(Number),
-      updated: expect.any(Number),
-    },
+    message1.params.entry,
+    message2.params.entry,
+    message3.params.entry,
   ]);
 
   entries = keyBy(response, "id");
@@ -68,42 +128,80 @@ test("create entry", async () => {
 });
 
 test("update entry", async () => {
-  await call("updateEntry", {
-    id: id1,
-    name: "name1_modified",
-    content: "content1_modified",
+  let message1;
+  [message1] = await Promise.all([
+    waitMessage(),
+    call("updateEntry", {
+      id: id1,
+      name: "name1_modified",
+      content: "content1_modified",
+    }),
+  ]);
+  expect(message1.type).toBe("notification");
+  expect(message1).toEqual({
+    jsonrpc: "2.0",
+    method: "updateEntry",
+    params: {
+      entry: {
+        id: id1,
+        name: "name1_modified",
+        content: "content1_modified",
+        created: expect.any(Number),
+        updated: expect.any(Number),
+      },
+    },
   });
-  await call("updateEntry", { id: id2, name: "only_name2_modified" });
-  await call("updateEntry", {
-    id: id3,
-    content: "only_content3_modified",
+
+  let message2;
+  [message2] = await Promise.all([
+    waitMessage(),
+    call("updateEntry", { id: id2, name: "only_name2_modified" }),
+  ]);
+  expect(message2.type).toBe("notification");
+  expect(message2).toEqual({
+    jsonrpc: "2.0",
+    method: "updateEntry",
+    params: {
+      entry: {
+        id: id2,
+        name: "only_name2_modified",
+        content: "content2",
+        created: expect.any(Number),
+        updated: expect.any(Number),
+      },
+    },
+  });
+
+  let message3;
+  [message3] = await Promise.all([
+    waitMessage(),
+    call("updateEntry", {
+      id: id3,
+      content: "only_content3_modified",
+    }),
+  ]);
+  expect(message3.type).toBe("notification");
+  expect(message3).toEqual({
+    jsonrpc: "2.0",
+    method: "updateEntry",
+    params: {
+      entry: {
+        id: id3,
+        name: "name3",
+        content: "only_content3_modified",
+        created: expect.any(Number),
+        updated: expect.any(Number),
+      },
+    },
   });
 
   const now = Date.now();
   const response = await call("listEntries");
 
   expect(response).toEqual([
-    {
-      id: id1,
-      name: "name1_modified",
-      content: "content1_modified",
-      created: expect.any(Number),
-      updated: expect.any(Number),
-    },
-    {
-      id: id2,
-      name: "only_name2_modified",
-      content: "content2",
-      created: expect.any(Number),
-      updated: expect.any(Number),
-    },
-    {
-      id: id3,
-      name: "name3",
-      content: "only_content3_modified",
-      created: expect.any(Number),
-      updated: expect.any(Number),
-    },
+    message1.params.entry,
+    message2.params.entry,
+    message3.params.entry,
   ]);
 
   response.forEach(function(entry) {
@@ -157,9 +255,41 @@ test("Error: method not found", async () => {
 });
 
 test("delete entry", async () => {
-  await call("deleteEntry", { id: id1 });
-  await call("deleteEntry", { id: id2 });
-  await call("deleteEntry", { id: id3 });
+  let message1;
+  [message1] = await Promise.all([
+    waitMessage(),
+    call("deleteEntry", { id: id1 }),
+  ]);
+  expect(message1.type).toBe("notification");
+  expect(message1).toEqual({
+    jsonrpc: "2.0",
+    method: "deleteEntry",
+    params: { id: id1 },
+  });
+
+  let message2;
+  [message2] = await Promise.all([
+    waitMessage(),
+    call("deleteEntry", { id: id2 }),
+  ]);
+  expect(message2.type).toBe("notification");
+  expect(message2).toEqual({
+    jsonrpc: "2.0",
+    method: "deleteEntry",
+    params: { id: id2 },
+  });
+
+  let message3;
+  [message3] = await Promise.all([
+    waitMessage(),
+    call("deleteEntry", { id: id3 }),
+  ]);
+  expect(message3.type).toBe("notification");
+  expect(message3).toEqual({
+    jsonrpc: "2.0",
+    method: "deleteEntry",
+    params: { id: id3 },
+  });
 
   const entries = keyBy(await call("listEntries"), "id");
 
